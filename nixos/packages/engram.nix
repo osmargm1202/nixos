357f52046918c24e5e511ccd8e3f1494e342c7e0
osmar <osmargm1202@gmail.com>
2026-05-28 08:37:17 +0000

    fix(nixos): build Engram with pinned deps

--- nixos/packages/engram.nix
+++ nixos/packages/engram.nix
@@ -1,4 +1,4 @@
-{ lib, buildGoModule, fetchFromGitHub }:
+{ lib, buildGoModule, fetchFromGitHub, git }:
 
 buildGoModule {
   pname = "engram";
@@ -11,8 +11,15 @@ buildGoModule {
     hash = "sha256-e0gjrUhO/5JpcUntbZJ2QasfSP2hP1N7RvRE4vBl5eE=";
   };
 
+  postPatch = ''
+    substituteInPlace go.mod \
+      --replace-fail "go 1.25.10" "go 1.25.9"
+  '';
+
   subPackages = [ "cmd/engram" ];
-  vendorHash = lib.fakeHash;
+  vendorHash = "sha256-O+pC4x4DKNUWr7Sx9iZOjK6a64wrQA4/lnjvkNLBX64=";
+
+  nativeCheckInputs = [ git ];
 
   meta = {
     description = "Engram CLI";
