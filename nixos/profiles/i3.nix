e290acd2e3a18287a1832ec260a8564ac5c52e06
osmar <osmargm1202@gmail.com>
2026-07-18 04:44:23 -0400

    feat: complete portable i3 profile

--- nixos/profiles/i3.nix
+++ nixos/profiles/i3.nix
@@ -1,69 +1,152 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  lib,
+  userName ? "osmarg",
+  ...
+}:
 
 {
-  services.xserver.enable = true;
-  services.xserver.windowManager.i3 = {
-    enable = true;
-    extraPackages = with pkgs; [ i3status ];
-  };
+  imports = [ ./printer.nix ];
 
+  services.xserver = {
+    enable = true;
+    displayManager.startx = {
+      enable = true;
+      generateScript = true;
+    };
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [ i3status ];
+    };
+  };
+  services.displayManager.defaultSession = "none+i3";
+  services.getty.autologinUser = userName;
+
+  programs.fish.loginShellInit = lib.mkAfter ''
+    if test (tty) = /dev/tty1; and not set -q DISPLAY
+      exec startx /etc/X11/xinit/xinitrc
+    end
+  '';
+
+  services.libinput.enable = true;
   security.polkit.enable = true;
   services.dbus.enable = true;
   services.gvfs.enable = true;
+  services.udisks2.enable = true;
+  services.upower.enable = true;
   services.gnome.gnome-keyring.enable = true;
+  services.power-profiles-daemon.enable = true;
+
   programs.dconf.enable = true;
+  security.pam.services.login.enableGnomeKeyring = true;
   programs.nautilus-open-any-terminal = {
     enable = true;
     terminal = "kitty";
   };
 
+  services.pulseaudio.enable = false;
+
   xdg.portal = {
     enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-    ];
-    config.common.default = [ "gtk" ];
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config = {
+      common.default = [ "gtk" ];
+      i3.default = [ "gtk" ];
+    };
+  };
+
+  xdg.terminal-exec = {
+    enable = true;
+    settings = {
+      i3 = [ "kitty.desktop" ];
+      default = [ "kitty.desktop" ];
+    };
+  };
+
+  xdg.mime = {
+    enable = true;
+    defaultApplications = {
+      "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+      "text/plain" = [ "org.gnome.TextEditor.desktop" ];
+      "text/markdown" = [ "org.gnome.TextEditor.desktop" ];
+      "text/x-markdown" = [ "org.gnome.TextEditor.desktop" ];
+      "application/pdf" = [ "org.gnome.Evince.desktop" ];
+      "image/png" = [ "org.gnome.Loupe.desktop" ];
+      "image/jpeg" = [ "org.gnome.Loupe.desktop" ];
+      "image/webp" = [ "org.gnome.Loupe.desktop" ];
+      "image/gif" = [ "org.gnome.Loupe.desktop" ];
+      "application/zip" = [ "org.gnome.FileRoller.desktop" ];
+      "application/x-tar" = [ "org.gnome.FileRoller.desktop" ];
+      "application/x-7z-compressed" = [ "org.gnome.FileRoller.desktop" ];
+      "text/html" = [ "chromium.desktop" ];
+      "application/xhtml+xml" = [ "chromium.desktop" ];
+      "x-scheme-handler/http" = [ "chromium.desktop" ];
+      "x-scheme-handler/https" = [ "chromium.desktop" ];
+    };
+  };
+
+  environment.sessionVariables = {
+    XDG_SESSION_TYPE = "x11";
+    XDG_SESSION_DESKTOP = "i3";
+    XDG_CURRENT_DESKTOP = "i3";
+    TERMINAL = "kitty";
   };
 
   environment.systemPackages = with pkgs; [
+    # Xorg session and window manager.
+    xorg.xorgserver
+    xorg.xinit
+    xorg.xauth
+    xorg.xrdb
+    xorg.xrandr
+    xorg.xinput
+    xorg.xset
+    xorg.xsetroot
+    xorg.setxkbmap
+    xorg.xkill
     i3
     i3lock-color
     polybar
     picom
 
+    # Launchers, notifications, wallpaper and X11 helpers.
     rofi
+    rofi-calc
+    clipmenu
     dunst
+    feh
+    arandr
+    xclip
 
+    # Desktop integration.
     xdg-utils
+    desktop-file-utils
     xdg-desktop-portal
     xdg-desktop-portal-gtk
-
-    xwinwrap
-    mpv
-    ffmpeg
-
-    kitty
-    chromium
-    nautilus
-    stow
-
+    shared-mime-info
     networkmanagerapplet
     blueman
     pavucontrol
-
     polkit_gnome
     gnome-keyring
     dex
     xss-lock
+    udiskie
+    usbutils
 
+    # User controls.
     flameshot
     brightnessctl
     pamixer
     playerctl
 
-    xclip
-    xorg.xset
-    xorg.xsetroot
-    xorg.setxkbmap
+    # Daily applications used by MIME defaults and bindings.
+    kitty
+    chromium
+    nautilus
+    gnome-text-editor
+    evince
+    loupe
+    file-roller
   ];
 }
---
+++ tests/i3-profile.bats.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+cd "$ROOT"
+
+fail() {
+  printf 'FAIL: %s\n' "$*" >&2
+  exit 1
+}
+
+for host in orgm lenovo ero jarq; do
+  grep -Eq "^[[:space:]]*${host}-i3[[:space:]]*=[[:space:]]*mkHost" flake.nix \
+    || fail "missing ${host}-i3"
+  grep -A4 -E "^[[:space:]]*${host}-i3[[:space:]]*=" flake.nix \
+    | grep -q 'profile = ./nixos/profiles/i3.nix' \
+    || fail "${host}-i3 does not use i3.nix"
+done
+
+[[ "$(nix eval .#nixosConfigurations.orgm-i3.config.services.xserver.enable 2>/dev/null)" == true ]] \
+  || fail 'Xserver disabled'
+[[ "$(nix eval .#nixosConfigurations.orgm-i3.config.services.xserver.displayManager.startx.enable 2>/dev/null)" == true ]] \
+  || fail 'startx disabled'
+[[ "$(nix eval .#nixosConfigurations.orgm-i3.config.services.xserver.displayManager.startx.generateScript 2>/dev/null)" == true ]] \
+  || fail 'xinitrc generation disabled'
+[[ "$(nix eval .#nixosConfigurations.orgm-i3.config.services.getty.autologinUser --raw 2>/dev/null)" == osmarg ]] \
+  || fail 'orgm autologin user'
+[[ "$(nix eval .#nixosConfigurations.jarq-i3.config.services.getty.autologinUser --raw 2>/dev/null)" == jarq ]] \
+  || fail 'jarq autologin user'
+
+printf 'PASS: i3 profile tests\n'
